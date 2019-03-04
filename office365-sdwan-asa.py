c188567ab381a4be3be35c2ec6824649fd753a7e
Jeremie Garnier <jegarnie@cisco.com>
2019-03-04 15:46:56 +0100

    minor code review: concatenation using format and .gitignore

--- office365-sdwan-asa.py
+++ office365-sdwan-asa.py
@@ -8,11 +8,6 @@ import uuid
 import socket
 import struct
 
-# required inputs
-interface=input('Name of the outgoing interface for office365 ex:outside2: ') or 'outside2'
-gateway=input('Enter the ip address of the gateway ex:1.1.1.1: ') or '1.1.1.1'
-trackedip=input('Enter an ip address for route tracking ex:8.8.8.8: ') or '8.8.8.8'
-
 #convert cidr to netmask
 def cidr_to_netmask(cidr):
 	network, net_bits = cidr.split('/')
@@ -22,34 +17,37 @@ def cidr_to_netmask(cidr):
 
 #get Microsoft Json File
 def webApiGet(methodName, instanceName, clientRequestId):
-	ws = "https://endpoints.office.com"
-	requestPath = ws + '/' + methodName + '/' + instanceName + '?clientRequestId=' + clientRequestId
+	requestPath = "{}/{}/{}?clientRequestId={}".format("https://endpoints.office.com",methodName,instanceName,clientRequestId)
 	request = urllib.request.Request(requestPath)
 	with urllib.request.urlopen(request) as response:
 		return json.loads(response.read().decode())
 
-clientRequestId = str(uuid.uuid4())
-endpointSets = webApiGet('endpoints', 'Worldwide', clientRequestId)
+def main():
+	# required inputs
+	interface=input('Name of the outgoing interface for office365 ex:outside2: ') or 'outside2'
+	gateway=input('Enter the ip address of the gateway ex:1.1.1.1: ') or '1.1.1.1'
+	trackedip=input('Enter an ip address for route tracking ex:8.8.8.8: ') or '8.8.8.8'
+	clientRequestId = str(uuid.uuid4())
+	endpointSets = webApiGet('endpoints', 'Worldwide', clientRequestId)
 
-#get the ip adresses
-flatIps = []
-for endpointSet in endpointSets:
-	if endpointSet['category'] in ('Optimize', 'Allow'):
-		ips = endpointSet['ips'] if 'ips' in endpointSet else []
-		ip4s = [ip for ip in ips if '.' in ip]
-		flatIps.extend([(ip) for ip in ip4s])
+	#get the ip adresses
+	flatIps = []
+	for endpointSet in endpointSets:
+		if endpointSet['category'] in ('Optimize', 'Allow'):
+			ips = endpointSet['ips'] if 'ips' in endpointSet else []
+			ip4s = [ip for ip in ips if '.' in ip]
+			flatIps.extend([(ip) for ip in ip4s])
 
-#generate configuration append file
-max=str(flatIps).count(',')
-routes = 'conf t\ntrack 123 rtr 123 reachability\nsla monitor 123\ntype echo protocol ipIcmpEcho '+ str(trackedip) + ' interface outside2\nsla monitor schedule 123 life forever start-time now\n'
-i = 0
+	#generate configuration append file
+	routes = "conf t\ntrack 123 rtr 123 reachability\nsla monitor 123\ntype echo protocol ipIcmpEcho {} interface outside2\nsla monitor schedule 123 life forever start-time now\n".format(trackedip)
 
-while i <= max :
-	subnet=(cidr_to_netmask(flatIps[i]))
-	routes = routes + 'route '+ interface +' '+ (subnet[0]) +' '+ (subnet[1]) + ' ' + gateway + ' track 123' +'\n'
-	i += 1
+	for i in range(0,str(flatIps).count(',')+1) :
+		subnet=cidr_to_netmask(flatIps[i])
+		routes = "{}route {} {} {} {} track 123\n".format(routes,interface,subnet[0],subnet[1],gateway)
 
-print(('\nThe configuration to append in your ASA is written in the file route-add-to-asa-for-office365.txt\n'))
-file = open('route-add-to-asa-for-office365.txt', "w")
-file.write(routes)
-file.close()
+	print('\nThe configuration to append in your ASA is written in the file route-add-to-asa-for-office365.txt\n')
+	with open('route-add-to-asa-for-office365.txt', "w") as f:
+		f.write(routes)
+
+if __name__ == "__main__":
+  main()
